595d4e0da8667b36dabd068271bf88e45bba6250
knahsirV <64799319+knahsirV@users.noreply.github.com>
2022-07-07 19:03:42 +0530

    finished README

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # News Archiver
-CNN and Fox are known for reporting with opposing viewpoints over US politics. This application allows users to view and visit the headlines of both news sources for any given date. The frontend is built with React, and the data is pulled with a webscraper built with NodeJS that autmatically runs once every hour and saves the data to a Firebase firestore. Access the [live site here: https://news-archiver-d0853.web.app/](https://news-archiver-d0853.web.app/)
+*This project was inspired by Fireship's video: <a href="https://www.youtube.com/watch?v=JTOJsU3FSD8" target="_blank">7 Full Stack App Ideas for Developers</a>.*
+<br /><br />
+CNN and Fox are known for reporting with opposing viewpoints over US politics. This application allows users to view and visit the headlines of both news sources for any given date, allowing them to compare at any moment the reporting from both sides. Access the
+<a href="https://news-archiver-d0853.web.app/" target="_blank">live site here</a>.
+
+## Components
+
+- **Frontend**: React app styled with TailwindCSS and deployed to Firebase
+- **Backend**: ExpressJS server hosted on Heroku that pulls headlines from CNN and FOX every hour and saves data to a Firestore DB. Webscraper built with Puppeteer.
+
+## Dev Proccess & Challenges:
+
+My first task was creating a NodeJS webscraper with Puppeteer that would visit the politics pages of both CNN and FOX, pull each headline as well as their corresponding URLs and image, and then save the scraped data to a firestore DB. Inspecting the webpages to scrape the needed content was initially cumbersome, but further research into how the document query selector interacts with the DOM simplified the proccess.
+<br/> <br/>
+After building the scraper, I needed a way of autmatically running the scraper. Though I initially wanted to use the built-in Firebase cloud functions and pub-sub, I quickly abandonded this path after learning that I would have to pay for this. I then turned to Heroku for hosting. I made a boilerplate ExpressJS server to host as well as quickly monitor the status of the program. I initially set up a cron job to automate the webscraper locally, but after finding out that cron jobs will not run when Heroku turns your server off, I learned how to set up and test scheduled tasks on Heroku. This was my first time deploying an automated task, so I had to research quite a bit on the most appropriate method for this situation.
+<br/> <br/>
+After setting up the backend, it was a matter of building a frontend that queried the database for the latest scrape from a given date. The design took some inspiration from <a href="https://dribbble.com/shots/8110794-News-Feed-App-Concept" target="_blank"> this dribble mockup</a>, though I had to considerably change it to fit larger screen widths.
---
+++ backend/NewsArchiver.js
@@ -0,0 +1,75 @@
+const puppeteer = require("puppeteer");
+const serviceAccount = require("./news-archiver-d0853-firebase-adminsdk-ncrnl-7b1b2933ce.json");
+const fs = require("firebase-admin");
+
+fs.initializeApp({
+  credential: fs.credential.cert(serviceAccount),
+});
+
+const db = fs.firestore();
+
+const newsDb = db.collection("news");
+
+cnnHeadlines = [];
+foxHeadlines = [];
+
+const scrapeNews = async () => {
+  console.log(`Starting Scrape #${Date.now().toString()}`);
+  const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+  console.log(`Scrape #${Date.now().toString()}: Launched browser`);
+  const page = await browser.newPage();
+  console.log(`Scrape #${Date.now().toString()}: Opened new page`);
+  await page.goto("https://edition.cnn.com/politics");
+  console.log(`Scrape #${Date.now().toString()}: Navigated to http://edition.cnn.com/politics`);
+  headings = await page.evaluate(() => {
+    headings_elements = document.querySelectorAll("h3 > a");
+    headings_array = Array.from(headings_elements);
+    media_elements = document.querySelectorAll(".media > a > img");
+    media_array = Array.from(media_elements).map((image) => image.getAttribute("src"));
+    console.log(`Scrape #${Date.now().toString()}: Scraped all necessary data from CNN`);
+    return headings_array.map((heading, index) => {
+      return {
+        heading: heading.text,
+        link: heading.href,
+        img: media_array[index],
+      };
+    });
+  });
+  cnnHeadlines = headings;
+  console.log(`Scrape #${Date.now().toString()}: CNN data pulled in correct format`);
+
+  await page.goto("https://www.foxnews.com/politics");
+  console.log(`Scrape #${Date.now().toString()}: Navigated to http://www.foxnews.com/politics`);
+  headings = await page.evaluate(() => {
+    headings_elements = document.querySelectorAll(".title > a");
+    media_elements = document.querySelectorAll("article > .m > a > picture > img");
+    media_array = Array.from(media_elements).map((image) => image.getAttribute("src"));
+    console.log(`Scrape #${Date.now().toString()}: Scraped all necessary data from Fox`);
+    return Array.from(headings_elements).map((heading, index) => {
+      return {
+        heading: heading.text,
+        link: heading.href,
+        img: media_array[index],
+      };
+    });
+  });
+  foxHeadlines = headings;
+  console.log(`Scrape #${Date.now().toString()}: CNN data pulled in correct format`);
+
+  await browser.close();
+  console.log(`Scrape #${Date.now().toString()}: Closed browser`);
+
+  console.log(`Scrape #${Date.now().toString()}:`);
+  console.log("CNN:");
+  console.log(cnnHeadlines);
+  console.log("FOX:");
+  console.log(foxHeadlines);
+
+  await newsDb.doc(Date.now().toString()).set({
+    cnn: cnnHeadlines,
+    fox: foxHeadlines,
+    date: fs.firestore.FieldValue.serverTimestamp(),
+  });
+  console.log(`Scrape #${Date.now().toString()}: Saved data to db\n`);
+};
+scrapeNews();
